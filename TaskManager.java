60b8be6eb96023bb55df0fcecc7a7e1364b3a715
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-05-26 00:12:30 +0700

    Step 4: CREATE(Add new data)

--- TaskManager.java
+++ TaskManager.java
@@ -4,4 +4,10 @@
   public class TaskManager{
       // Data Structure to hold the objects
       private List<Task> taskList = new ArrayList<>();
+
+      public void addTask(Task task){
+          taskList.add(task);
+          System.out.println("Task added successfully!");
+      }     
   }
+ 
