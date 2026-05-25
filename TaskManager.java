8ecf92e308b228e6cd94aa940e3997659cecf256
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-05-26 00:17:21 +0700

    Step 4.2: READ(View all data)

--- TaskManager.java
+++ TaskManager.java
@@ -8,6 +8,15 @@
       public void addTask(Task task){
           taskList.add(task);
           System.out.println("Task added successfully!");
-      }     
+      }
+      public void viewAllTasks(){
+          if (taskList.isEmpty()) {
+              System.out.println("No tasks available.");
+              return;
+          }
+          for (Task t : taskList) {
+              System.out.println(t.toString());
+          }
+      }  
   }
  
