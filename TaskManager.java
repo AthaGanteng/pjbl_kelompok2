fd19b680444b85394b064fc4c013d07d7389145f
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-05-26 00:19:11 +0700

    Step 4.3: UPDATE(Modify existing data)

--- TaskManager.java
+++ TaskManager.java
@@ -18,5 +18,15 @@
               System.out.println(t.toString());
           }
       }  
+      public void updateTask(int id, String newTitle){
+          for (Task t : taskList) {
+              if (t.getId() == id) {
+                  t.setTitle(newTitle);
+                  System.out.println("Task updated!");
+                  return;
+              }
+          }
+          System.out.println("Task not found.");
+      }
   }
  
