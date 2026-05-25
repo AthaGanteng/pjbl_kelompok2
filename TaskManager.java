f4b53dd406eb6715d70047125c9885b553d87271
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-05-26 00:20:34 +0700

    Step 4.4: DELETE(Remove data)

--- TaskManager.java
+++ TaskManager.java
@@ -28,5 +28,9 @@
           }
           System.out.println("Task not found.");
       }
+      public void deleteTask(int id){
+          taskList.removeIf(t -> t.getId() == id);
+          System.out.println("Task deleted (if existed).");
+      }
   }
  
